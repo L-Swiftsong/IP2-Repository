edf55c66001051ae9d65ad1190f8b635e446c6b3
Jinnes04 <125297175+Jinnes04@users.noreply.github.com>
2024-02-04 22:21:05 +0000

    all player movement

---
+++ IP2 Project/Assets/Code/Scripts/PlayerMovement.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float moveSpeed;
+    public Rigidbody2D rb;
+
+    private Vector2 moveDirection;
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInputs();
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void ProcessInputs()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+        if(Input.GetKeyDown("left shift"))
+        {
+            moveSpeed = 10;
+        }
+        if (Input.GetKeyUp("left shift"))
+        {
+            moveSpeed = 5;
+        }
+        
+
+        moveDirection = new Vector2(moveX, moveY).normalized;
+    }
+
+    void Move()
+    {
+        rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+    }
+}
