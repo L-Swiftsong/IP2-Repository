92041b04682d8f642f04e45a5ca2be395e4ce212
L-Swiftsong <acarey300@caledonian.ac.uk>
2024-02-25 14:43:16 +0000

    Dash Change & Inspector Prettying
    We have change how we are handling the dash from changing the player's speed to setting the velocity of the player's rigidbody. This has allowed us to have the player dash even when stationary.
    
    Furthermore, I have exposed some additional variables to the inspector and renamed some others, providing more clarity through their names and giving the designers more control over aspects of the dash such as how soon after dashing the stamina starts regenerating (That's it for the newly exposed variables).
    
    In addition to these changes, I have added [Header] and [Space] attributes to make the variables more organised in the inspector.

--- IP2 Project/Assets/Code/Scripts/PlayerMovement.cs
+++ IP2 Project/Assets/Code/Scripts/PlayerMovement.cs
@@ -5,121 +5,128 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    
-    public float moveSpeed;
-    public Rigidbody2D rb;
-    public bool sprint = false;
+    [Header("Movement")]
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private Rigidbody2D rb;
+    private Vector2 movementInput;
 
-    public float Stamina, MaxStamina;
     public static System.Action<float> OnStaminaChanged; // <float staminaPercentage>
     public static System.Action<float, float> OnStaminaValuesChanged; // <float maxStamina, float dashCost>
 
-    public float DashCost;
 
-    private Coroutine recharge;
-    public float ChargeRate;
+    [Header("Dashing")]
+    [SerializeField] private float dashSpeed;
+    [SerializeField] private float dashDuration = 0.5f;
+    private float dashDurationRemaining;
 
-    private Vector2 movementInput;
-    //private Vector2 moveDirection;
+    [SerializeField] private float dashCooldown = 0.25f;
+    private float dashCooldownRemaining;
+    [SerializeField] private float DashCost;
 
-    private float activeMoveSpeed;
-    public float dashSpeed;
+    private bool _isDashing;
 
-    public float dashLength = .5f, dashCooldown = 1f;
 
-    private float dashCounter;
-    private float dashCoolCounter;
-    bool isMoving;
+    [Header("Stamina")]
+    [SerializeField] private float Stamina;
+    [SerializeField] private float MaxStamina;
 
-    private void Start()
-    {
-        activeMoveSpeed = moveSpeed;
-        isMoving = false;
+    [Space(5)]
+    [SerializeField] private float ChargeRate;
+    private Coroutine dashRechargeCoroutine;
 
-        OnStaminaValuesChanged?.Invoke(MaxStamina, DashCost);
-    }
+    [SerializeField] private float staminaRegenerationDelay = 1f;
+    private float staminaRegenerationDelayRemaining;
 
-    public void OnMovementInput(InputAction.CallbackContext context)
-    {
-        movementInput = context.ReadValue<Vector2>().normalized;
 
-        if (context.started)
-        {
-            isMoving = true;
-        }
 
-        if (context.canceled)
-        {
-            isMoving = false;
-        }
-    }
-
-  
-
+    public void OnMovementInput(InputAction.CallbackContext context) => movementInput = context.ReadValue<Vector2>().normalized;
+    
     public void OnDashPressed(InputAction.CallbackContext context)
     {
         if (context.performed)
-        {
-            if (dashCoolCounter <= 0 && dashCounter <= 0 && Stamina >= DashCost && isMoving == true)
-            {
-                activeMoveSpeed = dashSpeed;
-                dashCounter = dashLength;
-                Stamina -= DashCost;
-
-                if (recharge != null)
-                    StopCoroutine(recharge);
-                recharge = StartCoroutine(RechargeStamina());
-            }
-        }
+            AttemptDash();
     }
 
-    void FixedUpdate()
-    {
-        Move();
-    }
+
+    private void Start() => OnStaminaValuesChanged?.Invoke(MaxStamina, DashCost);
 
     void Update()
     {
-        if (dashCounter > 0)
+        // If we are currently dashing, handle dashing stuff.
+        if (_isDashing)
         {
-            dashCounter -= Time.deltaTime;
+            // Decrement the dash duration remaining.
+            dashDurationRemaining -= Time.deltaTime;
 
-            if (dashCounter <= 0)
+            // If we are to stop dashing, do so.
+            if (dashDurationRemaining <= 0f)
             {
-                activeMoveSpeed = moveSpeed;
-                dashCoolCounter = dashCooldown;
+                _isDashing = false;
+                dashCooldownRemaining = dashCooldown;
             }
         }
+        // If we aren't dashing & our cooldown time hasn't elapsed, decrement the cooldown time remaining.
+        else if (dashCooldownRemaining > 0f)
+            dashCooldownRemaining -= Time.deltaTime;
+    }
+    void FixedUpdate() => Move();
 
-        if (dashCoolCounter > 0)
-        {
-            dashCoolCounter -= Time.deltaTime;
-        }
+
+    void Move()
+    {
+        // Don't process movement if we are dashing.
+        if (_isDashing)
+            return;
+
+        // Move by setting the player's velocity.
+        rb.velocity = movementInput * moveSpeed;
     }
 
+
+    private void AttemptDash()
+    {
+        // If we cannot dash for whatever reason, stop here.
+        if (_isDashing || dashCooldownRemaining > 0 || Stamina < DashCost)
+            return;
+
+        
+        // Start Dashing.
+        Vector2 dashDirection = (movementInput != Vector2.zero ? movementInput : (Vector2)transform.up).normalized;
+        rb.velocity = dashDirection * dashSpeed;
+
+        dashDurationRemaining = dashDuration;
+        _isDashing = true;
+
+
+        // Update Stamina.
+        Stamina -= DashCost;
+        OnStaminaChanged?.Invoke(Stamina / MaxStamina);
+
+        // Recharge the player's stamina.
+        if (dashRechargeCoroutine != null)
+            StopCoroutine(dashRechargeCoroutine);
+        dashRechargeCoroutine = StartCoroutine(RechargeStamina());
+    }
     private IEnumerator RechargeStamina()
     {
-        OnStaminaChanged?.Invoke(Stamina / MaxStamina);
-        
-        yield return new WaitForSeconds(1f);
+        // Don't start regenerating stamina for staminaRegenerationDelay seconds.
+        yield return new WaitForSeconds(staminaRegenerationDelay);
 
+        // Loop until we have fully regenerated our stamina.
         while (Stamina < MaxStamina)
         {
-            Stamina += ChargeRate / 10f;
-
+            // Increase our stamina, clamping to prevent exceeding our maximum.
+            Stamina += ChargeRate * Time.deltaTime;
             if (Stamina > MaxStamina)
             {
                 Stamina = MaxStamina;
             }
 
+            // Notify listeners that the stamina has changed.
             OnStaminaChanged?.Invoke(Stamina / MaxStamina);
 
-            yield return new WaitForSeconds(.1f);
+            // Wait until the next frame.
+            yield return null;
         }
-    }
-
-    void Move()
-    {
-        rb.velocity = movementInput * activeMoveSpeed;
     }
 }
