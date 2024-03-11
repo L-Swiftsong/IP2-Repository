29e3db835756bb4c992b21889be49826b6c30161
L-Swiftsong <acarey300@caledonian.ac.uk>
2024-03-11 15:17:20 +0000

    Updated Build Indexes
    I've ensured that the merge has left no conflicts and I have updated the Build Indexes to include the First, Second, and Tutorial levels

--- IP2 Project/Assets/Code/Scripts/Scene Management/SceneIndexes.cs
+++ IP2 Project/Assets/Code/Scripts/Scene Management/SceneIndexes.cs
@@ -3,5 +3,7 @@ public enum SceneIndexes
     PERSISTENT_SCENE = 0,
     TITLE_SCREEN = 1,
     TUTORIAL_SCENE = 2,
-    FIRST_LEVEL = 3
+    FIRST_LEVEL = 3,
+    SECOND_LEVEL = 4,
+    THIRD_LEVEL = 5
 }
