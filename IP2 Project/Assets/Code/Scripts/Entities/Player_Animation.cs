e9064d5b4b50dc5616ee70e9bb3a95b7815767f4
Mr-Eui <122097894+Mr-Eui@users.noreply.github.com>
2024-03-20 15:03:25 +0000

    Changed script to use rotation pivot
    The way this is done isn't great

--- IP2 Project/Assets/Code/Scripts/Entities/Player_Animation.cs
+++ IP2 Project/Assets/Code/Scripts/Entities/Player_Animation.cs
@@ -7,6 +7,7 @@ public class Player_Animation : MonoBehaviour
 {
     [SerializeField] private Animator playerAnim;
     [SerializeField] private PlayerController _playerController;
+    [SerializeField] private Transform rotationPivot;
 
     private Vector2 movementInput;
 
@@ -19,20 +20,42 @@ public class Player_Animation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       
+        // Look up
+        if (rotationPivot.transform.rotation.z > -0.5f && rotationPivot.transform.rotation.z < 0.5f)
+        {
+            playerAnim.SetFloat("YInput", 1);
+        }
 
+        // Look right
+        if (rotationPivot.transform.rotation.z > -0.7f && rotationPivot.transform.rotation.z < -0.5f)
+        {
+            playerAnim.SetFloat("XInput", 1);
+        }
+        
+        // Look down
+        if (rotationPivot.transform.rotation.z > 0.3f && rotationPivot.transform.rotation.z < 0.7f)
+        {
+            playerAnim.SetFloat("YInput", -1);
+        }
 
+        // Look left
+        if (rotationPivot.transform.rotation.z > 0.5f && rotationPivot.transform.rotation.z < -0.5f)
+        {
+            playerAnim.SetFloat("XInput", -1);
+        }
+
+        Debug.Log(rotationPivot.transform.rotation.z);
     }
 
-    public void OnMovementInput(InputAction.CallbackContext context)
+    /*public void OnMovementInput(InputAction.CallbackContext context)
     {
         movementInput = context.ReadValue<Vector2>().normalized;
         playerAnim.SetFloat("XInput", movementInput.x);
-        playerAnim.SetFloat("YInput", movementInput .y);
+        playerAnim.SetFloat("YInput", movementInput.y);
 
         
 
-    }
+    }*/
 
 
 }
