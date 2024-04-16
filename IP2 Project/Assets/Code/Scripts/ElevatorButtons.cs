de80cb22d7feb324a138664e5cace722354cb51a
Jinnes04 <125297175+Jinnes04@users.noreply.github.com>
2024-04-16 01:04:02 +0100

    Elevator

---
+++ IP2 Project/Assets/Code/Scripts/ElevatorButtons.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevatorButtons : MonoBehaviour
+{
+    public bool button1Pressed;
+
+    public bool button2Pressed;
+
+    public bool button3Pressed;
+
+    private void Start()
+    {
+        button1Pressed = false;
+        button2Pressed = false;
+        button3Pressed = false;
+    }
+
+
+    public void Button1()
+    {
+        button1Pressed = true;
+    }
+
+    public void Button2()
+    {
+        button2Pressed = true;
+    }
+
+    public void Button3()
+    {
+        button3Pressed = true;
+    }
+}
